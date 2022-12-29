b0d72b10476332787018667e8b0e7804214dd527
Olena Bieletska <belshel12@gmail.com>
2022-12-29 14:09:57 +0200

    update-03-feedback-js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+  submitBtn: document.querySelector('.feedback-form button'),
+};
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formValue = {};
+
+updateForm();
+
+refs.form.addEventListener('input', throttle(onInputForm, 500));
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onInputForm(evt) {
+  formValue[evt.target.name] = evt.target.value;
+  console.log(formValue);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formValue));
+}
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function updateForm() {
+  const savedValues = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedValues) {
+    const parsedValues = JSON.parse(savedValues);
+
+    refs.email.value = parsedValues.email || '';
+    refs.message.value = parsedValues.message || '';
+  }
+}
