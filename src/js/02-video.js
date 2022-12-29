136d884c70549b20dc5b3f55a747a8794df0580d
Olena Bieletska <belshel12@gmail.com>
2022-12-29 13:58:31 +0200

    update-02-videos-js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+updatePlayer();
+
+player.on('timeupdate', throttle(callback, 1000));
+
+function callback(e) {
+  localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+}
+
+function updatePlayer() {
+  const currentTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  player.setCurrentTime(currentTime);
+}
